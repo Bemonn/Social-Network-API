80942b90295612ebfba4f2009f83f90faacd00a5
Bemon <tristan2107b@gmail.com>
2023-09-27 20:09:56 +1000

    coded models and merged thought/reaction.js into just Thought.js

---
+++ models/Thought.js
@@ -0,0 +1,71 @@
+// Importing mongoose library
+const mongoose = require('mongoose');
+
+// Defining the schema for reactions
+const reactionSchema = new mongoose.Schema(
+  {
+    reactionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+// Defining the schema for thoughts
+const thoughtSchema = new mongoose.Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// Defining a virtual property to get the count of reactions
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+// Creating the Thought model
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+// Exporting Thought model
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,47 @@
+// Importing mongoose library
+const mongoose = require('mongoose');
+
+// Defining user schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    match: [/.+@.+\..+/, 'Please enter a valid email address'],
+  },
+  thoughts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Thought',
+    },
+  ],
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+},
+{
+  toJSON: {
+    virtuals: true,
+  },
+  id: false,
+});
+
+// Adding virtual property friendCount to schema, to compute length of friends array
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+// Creating the User model using mongoose.model
+const User = mongoose.model('User', userSchema);
+
+// Exporting User model
+module.exports = User;
